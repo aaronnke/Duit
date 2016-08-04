04040379a671ced9e127d46a1f52a9be8b310975
Leo <leo@Leos-MacBook-Pro.local>
2016-08-04 19:22:31 +0800

    bank created

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  resources :bank_accounts
+
+  # get "bankaccounts/id" => "bankaccounts/show", as: "bank_accounts_path"
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
---
+++ test/controllers/bank_account_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class BankAccountControllerTest < ActionController::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
