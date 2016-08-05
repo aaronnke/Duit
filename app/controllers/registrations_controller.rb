250e8cc88826b560a8350671da4f9c0f862f6266
nextacademy <dteohyl@gmail.com>
2016-08-05 18:16:50 +0800

    Groups v1.1 : added avatar strong_params

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,10 +1,10 @@
 class RegistrationsController < Devise::RegistrationsController
 
 	def sign_up_params
-		params.require(:user).permit(:name, :email, :password, :password_confirmation, :remember_me)
+		params.require(:user).permit(:name, :email, :password, :password_confirmation, :remember_me, :avatar)
 	end 
 
 	def account_update_params
-    	params.require(:user).permit(:name,:email, :password, :password_confirmation, :current_password)
+    	params.require(:user).permit(:name,:email, :password, :password_confirmation, :current_password, :avatar)
   	end
 end
