a648b83b1b4e4a5c2c5bbf15b57fc911419fa209
Leo <leo@Leos-MacBook-Pro.local>
2016-08-07 13:26:29 +0800

    Installed gem googlevisualr, ran a test to display the chart

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,26 @@ class PagesController < ApplicationController
 
   def profile
 
+  data_table = GoogleVisualr::DataTable.new
+  data_table.new_column('string', 'Type' )
+  data_table.new_column('number', 'Expenses')
+
+  formatter = new GoogleVisualr::DataTable.new({decimalSymbol: ',',groupingSymbol: '.', negativeColor: 'red', negativeParens: true, prefix: '$ '});
+
+
+# Add Rows and Values
+data_table.add_rows([
+    ['Food', formatter.format(1000)],
+    ['Entertainment', formatter.format(1170)],
+    ['Mortgage', formatter.format(660)],
+    ['Student Loan', formatter.format(1030)]
+])
+option = { width: 400, height: 240, pieHole: 0.6,legend: 'none',pieSliceText: 'none'}
+
+@chart = GoogleVisualr::Interactive::PieChart.new(data_table, option)
+
+
+    
     unless current_user.bank_accounts.empty?
       if current_user.bank_accounts.first.budgets.empty?
         @budget = current_user.bank_accounts.first.budgets.new
