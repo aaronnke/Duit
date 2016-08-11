763257bf6b54523cf65ad4cfc9c67d0b386cedf7
Leo <leo@Leos-MacBook-Pro.local>
2016-08-11 14:56:50 +0800

    even latest here

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -6,11 +6,19 @@ class TransactionsController < ApplicationController
     unless @tag.nil?
       @transaction = Transaction.new(tag_id: @tag.id, amount: params[:transaction][:amount], bank_account_id: params[:bank_account_id], description: params[:transaction][:description])
       @transaction.save
-      redirect_to user_dashboard_path(current_user.id)
+
+      respond_to do |format|
+        format.html { redirect_to user_dashboard_path(current_user.id) }
+        format.js
+      end
 
     else
       flash[:notice] = "Tag doesn't exist. Please refer to tags from your budget."
       redirect_to(:back)
+      respond_to do |format|
+        format.html { redirect_to(:back) }
+        format.js
+      end
       # redirect_to user_dashboard_path(current_user.id)
     end
 
